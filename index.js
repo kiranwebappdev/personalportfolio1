1550c20d9ad880fd1afc025126752b4d26b008f6
Aamon <123532887+kiranwebappdev@users.noreply.github.com>
2024-01-16 17:40:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $('#button').click(function () { 
+        $('#box').fadeOut();
+    });
+});
+
+$(document).ready(function () {
+    $('.Menu').click(function (e) { 
+        e.preventDefault();
+        $('.side-menu').slideToggle();
+    });
+});
+
+
+
+
+
+
+
+
+/** 
+ * 
+ */
+
+
